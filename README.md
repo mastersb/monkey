6d2590ac63c857be7ea1bc660971d2d784f10ba3
Benjamin Masters <bencmasters@gmail.com>
2026-04-04 08:57:21 -0400

    tracing envar

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # monkey
 Monkey Programming Language interpreter from https://interpreterbook.com
+
+Run the REPL with parser tracing enabled by setting `TRACE`, for example:
+
+```sh
+TRACE=1 go run main.go
+```
--- parser/parser_tracing.go
+++ parser/parser_tracing.go
@@ -2,12 +2,20 @@ package parser
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"strings"
 )
 
 var traceLevel int = 0
+var traceEnabled = readTraceEnabled()
 
 const traceIdentPlaceholder string = "\t"
+
+func readTraceEnabled() bool {
+	enabled, err := strconv.ParseBool(os.Getenv("TRACE"))
+	return err == nil && enabled
+}
 
 func identLevel() string {
 	return strings.Repeat(traceIdentPlaceholder, traceLevel-1)
@@ -21,12 +29,18 @@ func incIdent() { traceLevel = traceLevel + 1 }
 func decIdent() { traceLevel = traceLevel - 1 }
 
 func trace(msg string) string {
+	if !traceEnabled {
+		return msg
+	}
 	incIdent()
 	tracePrint("BEGIN " + msg)
 	return msg
 }
 
 func untrace(msg string) {
+	if !traceEnabled {
+		return
+	}
 	tracePrint("END " + msg)
 	decIdent()
 }
