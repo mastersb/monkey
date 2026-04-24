7ef2c5aa4617d0ee4b37db96f5ae3225974b157a
Benjamin Masters <bencmasters@gmail.com>
2026-04-24 19:37:48 -0400

    evaluation

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/mastersb/monkey/evaluator"
 	"github.com/mastersb/monkey/lexer"
+	"github.com/mastersb/monkey/object"
 	"github.com/mastersb/monkey/parser"
 )
 
@@ -26,6 +28,7 @@ const MONKEY_FACE = `            __,__
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -44,12 +47,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
-
-		// for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-		// 	fmt.Fprintf(out, "%+v\n", tok)
-		// }
+		evaluated := evaluator.Eval(program, env)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
